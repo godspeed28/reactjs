8e70cdf2fcc63c2d2701ca7203909267958a6a8b
godspeed28 <albertog4taz28@gmail.com>
2025-07-11 20:25:03 +0700

    navbar dengan react + bootstrap

---
+++ navbar-6/src/components/Navbar.jsx
@@ -0,0 +1,73 @@
+import logo from "../assets/react.svg";
+import styles from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <NavLink className="navbar-brand" to="/">
+          <img src={logo} alt="ReactJS" width="30" />
+          &nbsp; React
+        </NavLink>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                end
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? `${styles.active}` : "")
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active" : "")
+                }
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/services"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active" : "")
+                }
+              >
+                Services
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/contact"
+                className={({ isActive }) =>
+                  "nav-link" + (isActive ? " active" : "")
+                }
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ navbar-6/src/pages/Services.jsx
@@ -0,0 +1,8 @@
+export default function Services() {
+  return (
+    <div>
+      <h1>Halaman Services</h1>
+      <p>Ini adalah halaman services.</p>
+    </div>
+  );
+}
